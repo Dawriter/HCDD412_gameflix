c8706bbcb7e70c953144581aa654336ab55a9ae1
Helen Bongers <dawriterdarkdays@gmail.com>
2025-12-04 20:20:48 -0500

    Docker files added

--- src/main/java/com/example/gameflix/service/AccountService.java
+++ src/main/java/com/example/gameflix/service/AccountService.java
@@ -2,9 +2,15 @@ package com.example.gameflix.service;
 
 import com.example.gameflix.model.Account;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public interface AccountService {
+
+    public boolean validateUser(Account account);
+
     List<Account> getAllAccounts();
     void saveAccount(Account account);
     Account getAccountById(Long id);
